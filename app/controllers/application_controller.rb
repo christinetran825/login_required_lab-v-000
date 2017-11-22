60a9d225ce19aef1aa789ea833cd65c72c665c8a
Christine Tran <christinetran825@gmail.com>
2017-11-22 03:49:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    if !session[:name].present
+    if !session[:name].present?
       session[:name] == nil
     else
       sessions[:name]
