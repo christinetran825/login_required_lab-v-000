0c8ce88638f3f7b6dfe2a2e1195f5b2a5665856f
Christine Tran <christinetran825@gmail.com>
2017-11-22 04:11:52 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -12,7 +12,7 @@ class SecretsController < ApplicationController
 private
 
   def require_login
-    redirect_to new_session_path unless session.include? :name
+    redirect_to new_sessions_path unless session.include? :name
   end
 
 
