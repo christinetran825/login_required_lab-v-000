d945d196907ffd61353d0d558257841470a76bbd
Christine Tran <christinetran825@gmail.com>
2017-11-22 04:12:04 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,10 +3,6 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-    # if !sessions[:name].present?
-    #   redirect_to '/sessions/new'
-    # else
-    #   redirect_to
   end
 
 private
